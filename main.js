e2f63f3f42d5c24586c5d1471f920e688838c611
T-connect <63002972+T-connect@users.noreply.github.com>
2021-05-14 12:58:18 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+var vm = new Vue({
+    el: "#app",
+
+    data: {
+        movies: [],
+        details: false,
+        home: true
+    },
+
+    methods: {
+        getMovies(){
+          axios.get("https://api.artic.edu/api/v1/artworks")
+            .then((response) => {
+                this.movies = (response.data.data);
+                
+                
+            })
+        },
+
+
+        convertTime(time){
+             return  moment(time).format(" MMMM Do YYYY");
+            },
+
+             moreDetails(){
+                this.home = false;
+                this.details = true
+                },
+             
+
+            close(){
+                this.details = false;
+                this.home = true
+            }
+    },
+
+    mounted(){
+        this.getMovies();
+        
+    },
+  
+  
+})
+
+// var vm = new Vue({
+//     el: "#app2",
+//     data: {
+//         movies: [],
+//         buyArt: false
+//     },
+
+//     methods: {
+//     getMovies(){
+//         axios.get("https://api.artic.edu/api/v1/artworks")
+//           .then((response) => {
+//               this.movies = (response.data.data);
+              
+              
+//           })
+//       },
+
+//       buy(){
+//           this.buyArt = true
+//       }
+//     },
+
+//     mounted(){
+//         this.getMovies();
+        
+//     },
+// })
